e95dda07366406474594e804c9aa427d3fd7da6c
zaryabprg-cell <zaryabprg@gmail.com>
2025-08-07 10:51:45 +0500

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,18 @@
+def calculator():
+    print("Simple Calculator")
+    a = float(input("Enter first number: "))
+    op = input("Enter operator (+, -, *, /): ")
+    b = float(input("Enter second number: "))
+
+    if op == '+':
+        print("Result:", a + b)
+    elif op == '-':
+        print("Result:", a - b)
+    elif op == '*':
+        print("Result:", a * b)
+    elif op == '/':
+        print("Result:", a / b if b != 0 else "Cannot divide by zero.")
+    else:
+        print("Invalid operator")
+
+calculator()
